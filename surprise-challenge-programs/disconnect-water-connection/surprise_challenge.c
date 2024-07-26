7cee4a038bfb8aa298c4051ed3fb6b1a36ff0406
VedantGithub123 <vedantnehete@gmail.com>
2024-07-26 14:13:29 -0400

    Added Second Day & Surprise Challenge Ideas

---
+++ surprise-challenge-programs/disconnect-water-connection/surprise_challenge.c
@@ -0,0 +1,8 @@
+void supriseChallenge()
+{
+    // For the right water connection, replace the moving forward after the wall square to the following line
+    moveNoSyncMinspeedParam(100, 100, 735, 0.2, 0.2, true, RELDEG);
+
+    // For the left water connection replace the moving forward after the wall square to the following line
+    moveNoSyncMinspeedParam(100, 100, 703, 0.2, 0.2, true, RELDEG);
+}
