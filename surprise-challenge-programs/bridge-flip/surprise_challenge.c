65a62683d81b18d96133f6b31b573ed9d0a5663b
VedantGithub123 <vedantnehete@gmail.com>
2024-08-02 18:37:11 -0400

    Finished Bridge Surprise Challenge + Tuning Far Side

--- surprise-challenge-programs/bridge-flip/surprise_challenge.c
+++ surprise-challenge-programs/bridge-flip/surprise_challenge.c
@@ -1,7 +1,7 @@
 void surpriseChallenge()
 {
     // Add the following code to the very end of the program
-    moveArmAbs(50, defaultArmMinspeed, defaultArmMinspeed, 241, defaultAcc, defaultAcc, LIFT);
+    moveArmAbs(50, defaultArmMinspeed, defaultArmMinspeed, 220, defaultAcc, defaultAcc, LIFT);
 	moveArmAbs(50, defaultArmMinspeed, defaultArmMinspeed, -104, defaultAcc, defaultAcc, CLAW);
 	moveNoSyncParam(100, 100, defaultMinspeed, defaultMinspeed, 594, defaultAcc, defaultAcc, true, RELDEG);
 	turn2MotorAccMinspeed(20, defaultMinspeed, defaultMinspeed, 115, defaultAcc, defaultAcc, true);
@@ -16,7 +16,7 @@ void surpriseChallenge()
 	setSpeed(0, 0);
 	resetRelative();
 	moveNoSyncParam(100, 100, defaultMinspeed, defaultMinspeed, 587, 0.2, 0.2, true, RELDEG);
-	turn1MotorAccMinspeed(40, defaultMinspeed, defaultMinspeed, 90, defaultAcc, defaultAcc, true, LEFT);
+	turn1MotorAccMinspeed(40, defaultMinspeed, defaultMinspeed, 92, defaultAcc, defaultAcc, true, LEFT);
 	moveSenseOneSensorSimple(20, 20, defaultMinspeed, true, 3, WHITE, COLOR);
 	setArmSpeed(LIFT, -100);
 	sleep(300);
